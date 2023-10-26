27d5ac146ec101fb4df0d69e8c92013a738c959d
Mahmoud12309 <122573299+Mahmoud12309@users.noreply.github.com>
2023-10-26 21:24:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,54 @@
-const bar = document.querySelector("header .bar");
 const nav = document.querySelector("header nav");
-const close = document.querySelector("header nav .close");
+const li = document.querySelectorAll("header nav ul li a");
 
-bar.addEventListener("click", () => {
-    nav.classList.add("active");
+li.forEach(e => {
+    e.addEventListener("click", () => {
+        // إزالة الفئة "active" من العنصر الحالي
+        const activeElement = document.querySelector("header nav ul li a.active");
+        if (activeElement) {
+            activeElement.classList.remove("active");
+        }
+
+        // إضافة الفئة "active" إلى العنصر الذي تم النقر عليه
+        e.classList.add("active");
+        nav.classList.add("active");
+
+        let ewidth = e.offsetWidth;
+        console.log(ewidth);
+        nav.style.setProperty('--before-width', ewidth + "px");
+
+        // تحديد الخصائص المخصصة بناءً على الفئة
+        if (e.classList.contains("ts")) {
+
+            nav.style.setProperty('--before-right', 0 + "px");
+
+        } else if (e.classList.contains("seo")) {
+
+            nav.style.setProperty('--before-right', 74 + "px");
+
+        } else if (e.classList.contains("pr")) {
+
+            nav.style.setProperty('--before-right', 280 + "px");
+
+        } else if (e.classList.contains("yc")) {
+
+            nav.style.setProperty('--before-right', 340+ "px");
+
+        }
+    });
 });
 
-close.addEventListener("click", () => {
-    nav.classList.remove("active");
-});
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+
+
+// const bar = document.querySelector("header .bar");
+// bar.addEventListener("click", () => {
+//     nav.classList.add("active");
+// });
+
+// close.addEventListener("click", () => {
+//     nav.classList.remove("active");
+// });
+
+// bar.addEventListener("click", () => {
+//     nav.classList.toggle("active");
+// });
