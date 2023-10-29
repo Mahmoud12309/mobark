a324e291a0f4d125879faef0b7bc521dcbb28cbc
Mahmoud12309 <122573299+Mahmoud12309@users.noreply.github.com>
2023-10-29 21:42:18 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ li.forEach(e => {
         nav.classList.add("active");
 
         let ewidth = e.offsetWidth;
-        console.log(ewidth);
+
         nav.style.setProperty('--before-width', ewidth + "px");
 
         // تحديد الخصائص المخصصة بناءً على الفئة
@@ -32,7 +32,7 @@ li.forEach(e => {
 
         } else if (e.classList.contains("yc")) {
 
-            nav.style.setProperty('--before-right', 340+ "px");
+            nav.style.setProperty('--before-right', 340 + "px");
 
         }
     });
@@ -49,3 +49,45 @@ bar.addEventListener("click", () => {
 close.addEventListener("click", () => {
     nav.classList.remove("active");
 });
+
+
+let honeffect = document.querySelector(".landing .container .text h1 .text-h1");
+var texthone = "(عون-Aoun) ";
+let i = 0;
+let im = texthone.length - 1;
+let writing = true; // تتحقق من حالة الكتابة أو الحذف
+
+window.addEventListener("load", () => {
+    let set = setInterval(() => {
+        if (writing) {
+            honeffect.textContent += texthone[i];
+            i++;
+            if (i > im) {
+                writing = false;
+                i = im;
+            }
+        } else {
+            honeffect.textContent = honeffect.textContent.slice(0, -1);
+            i--;
+            if (i < 0) {
+                writing = true;
+                i = 0;
+            }
+        }
+    }, 250);
+});
+
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
